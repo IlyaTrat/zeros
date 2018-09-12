19af156de98a2380c0d55927a5c5939bcda9889c
Трацевский Илья <deeo-sama@hotmail.com>
2018-09-12 23:46:23 +0300

    Done with factorials

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  zeros = 0;
+  for(let i = 1; i < Math.pow(number, 0.2); i++) {
+    zeros += Math.floor(number / Math.pow(5, i));
+  }
+  return zeros;
 }
