40744ec1ec798f72fe70de5e94b8723cc77093b2
Трацевский Илья <deeo-sama@hotmail.com>
2018-09-13 18:29:05 +0300

    Well -_-

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 module.exports = function getZerosCount(number) {
   zeros = 0;
-  for(let i = 1; i < Math.pow(number, 0.2); i++) {
+  for(let i = 1;  Math.pow(5, i) <= number; i++) {
     zeros += Math.floor(number / Math.pow(5, i));
   }
   return zeros;
